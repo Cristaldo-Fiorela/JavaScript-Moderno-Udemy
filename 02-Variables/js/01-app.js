8513fda59f0585bc36a2b8113fdb0f13ca22497f
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-07 23:54:57 -0300

    ejercicios variables p1

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,29 @@
+// Inicializar una variante con un valor
+var producto = "Monitor de 20 pulgadas";
+console.log(producto);
+
+//Las variables se pueden reasignar
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+
+//Se puede iniciar sin valor y asignarlo despues
+var disponible;
+disponible = true;
+disponible = false;
+
+//Iniciar multiples variables
+var categoria = "Computadoras",
+    narco = "Marca famosa",
+    calificacion = 5;
+
+//Los nombres de las variables no pueden iniciarlizar con numeros
+var 99dias;
+var dias99;
+
+var 01_;
+var _01;
+var _usuario;
+var __usuario;
+
+var nombreProducto; // camelCase
+var nombre_producto // 
