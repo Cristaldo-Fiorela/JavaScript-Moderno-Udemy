0b072fee57e8b590cbc9d6ba4c930bcfe4b2d8f9
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-08 00:15:56 -0300

    ejercicios de variables let

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,16 @@
+let producto = 'tablet';
+
+// Permite reasignar el valor cuantas veces quedamos
+
+producto = "Monitor";
+producto = 20;
+producto = false;
+producto = null;
+
+console.log(producto)
+
+//Inicializada sin valor inicial
+let precio;
+precio = 300
+
+console.log(precio)
