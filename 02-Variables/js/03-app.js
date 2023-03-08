48e4ef2cec7a9964950537c4ef2144d01e1d392d
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-08 00:20:27 -0300

    ejercicios de variables const

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,10 @@
+const PRODUCTO = 'Tablet';
+
+// No puede ser reasignado
+producto = 'monitor'; // da error
+
+console.log(producto);
+
+// No pueden ser inicializados sin valores.
+// const PRECIO;
+console.log(precio);
