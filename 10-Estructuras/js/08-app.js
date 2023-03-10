383a568a1f5eafa4667a949f9a3a4e534da0b5f1
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-10 16:10:14 -0300

    buenas practicas de estructuras de control

---
+++ 10-Estructuras/js/08-app.js
@@ -0,0 +1,30 @@
+// Buenas practicas
+
+const autenticado = true;
+
+if(autenticado) {
+    console.log('El usuario esta autenticado');
+}
+
+// Importante el orden de las condiciones
+const puntaje = 500;
+
+if(puntaje > 400) {
+    console.log('Excelente!!');
+} else if ( puntaje > 300 ) {
+    console.log('Buen puntaje... felicidades');
+}
+
+// utilizacion de funcioens y return
+
+
+function revisarPuntaje() {
+    if(puntaje > 400) {
+        console.log('Excelente!!');
+        return;
+    } 
+    if ( puntaje > 300 ) {
+        console.log('Buen puntaje... felicidades');
+        return;
+    }
+}
