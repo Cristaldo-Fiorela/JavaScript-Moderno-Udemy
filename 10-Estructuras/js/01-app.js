f3561d3a3321f2aafb54e64fdda673b1dc1caa66
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-10 15:31:59 -0300

    creacion de if

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,16 @@
+// Creando if
+const puntaje = 1000;
+
+
+if(puntaje == 1000) {
+    console.log('Si es igual');
+} else {
+    console.log('No es igual');
+}
+
+// Si tiene una sola linea de codigo podes borrar las llaves del else
+if(puntaje == 1000) 
+    console.log('Si es igual');
+    else 
+    console.log('No es igual');
+
