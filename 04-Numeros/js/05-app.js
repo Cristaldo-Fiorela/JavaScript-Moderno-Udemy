4bd7a5f77e5e874c3ed37933fda5a0fff503ec7f
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-09 23:52:37 -0300

    ejercicios modulo numeros parte 2

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,8 @@
+// Incrementos y decrementos 
+
+let puntaje = 5;
+
+puntaje -= 3; // resta 3
+puntaje +=10; // suma 10
+
+console.log(puntaje); // 12
---
+++ 04-Numeros/js/06-app.js
@@ -0,0 +1,23 @@
+//Convertir strings a numeros...
+
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = "Uno";
+const numero4 = 20.2;
+
+console.log(numero1);
+// Convierte de string a numero entero
+console.log(Number.parseInt(numero1));
+// Convierte en un numero florante, con una fraccion
+console.log(Number.parseFloat(numero2));
+// NAN - no es un numero
+console.log(Number.parseInt(numero3)); 
+
+
+// Revisar si un numero es entero o no 
+
+console.log(Number.isInteger(numero4));
+console.log(Number.isInteger(numero3));
+
+
+
