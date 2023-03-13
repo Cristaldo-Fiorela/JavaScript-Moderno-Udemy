40687f3c20cd16d3e3707eac21ce444841cbee6a
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-13 16:15:58 -0300

    .some e .includes, diferencias

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -8,3 +8,29 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ]
+
+// Comprobar si un valor existe en un arreglo
+
+meses.forEach(mes => {
+    if( mes == 'Enero') {
+        console.log('Enero si existe');
+    }
+})
+
+// Revisa que un valor exista en un arreglo. No soporta arreglo de objetos
+const resultado = meses.includes('Enero'); // true
+const resultado2 = meses.includes('Diciembre'); // false
+console.log(resultado)
+
+// En un arreglo de objetos se utiliza .some
+const existe = carrito.some( producto => {
+    return producto.nombre === 'Celular'
+})
+console.log(existe);
+
+//En un arreglo tradicional con .some
+const existe2 = meses.some (mes => mes === 'Febrero');
+console.log(existe2)
+
+
+// .some soporta arreglo y arreglo de objetos
