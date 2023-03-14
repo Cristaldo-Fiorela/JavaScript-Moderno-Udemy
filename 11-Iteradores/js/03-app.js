cd0a140dcd9c4340038b7fed6b716404977eb89f
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-14 17:59:39 -0300

    ejercicios Fizz Buzz

--- 11-Iteradores/js/03-app.js
+++ 11-Iteradores/js/03-app.js
@@ -1,8 +1,9 @@
-// Reto Fizz Buzz
-
-// todos los multiplos de 3 imprimen fizz... 3 6 9 12
-// todos los multiplos de 5 imprimen buzz... 5 10 15 20
-// todos los multiplos de 3 y 5 imprimen  FIZZBUZZ
+/*
+RETO FIZZ BUZZ
+todos los multiplos de 3 imprimen fizz... 3 6 9 12
+todos los multiplos de 5 imprimen buzz... 5 10 15 20
+todos los multiplos de 3 y 5 imprimen  FIZZBUZZ
+*/
 
 for(let i = 0;  i < 100; i++) {
     if ( i % 15 === 0) { // OTRA CONDICION ( i % 3 === 0 && i % 5 === 0) 
