9ea13c89c5c03ccdc17edb49d150f694cc96f7f7
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-15 15:25:03 -0300

    comienzo de DOM con palabra document

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,25 @@
+/*
+Seleccionando elementos con la palabra reserveda document
+document hace referencia a todo nuestro documento html
+*/
+let elemento;
+
+elemento = document;
+elemento = document.all; // estan obsoletos
+elemento = document.head;
+elemento = document.body;
+elemento = document.domain; // estan obsoletos
+elemento = document.forms;
+elemento = document.forms[0];
+elemento = document.forms[0].id;
+elemento = document.forms[0].method;
+elemento = document.forms[0].classList;
+elemento = document.forms[0].action;
+elemento = document.links;
+elemento = document.links[4];
+elemento = document.links[4].classList;
+elemento = document.links[4].className;
+elemento = document.images;
+elemento = document.scripts;
+
+console.log(elemento)
