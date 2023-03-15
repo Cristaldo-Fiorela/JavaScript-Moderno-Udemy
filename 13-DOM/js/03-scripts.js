a0bc8dd099853546b971932ca970a24c47a32bde
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-15 15:32:27 -0300

    tomar elemento por su id

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,7 @@
+// Tomar un elemento por ID
+const formulario = document.getElementById('formulario');
+console.log(formulario);
+
+// Si no existe
+const noExiste = document.getElementById('no-existe');
+console.log(noExiste) // <== null
