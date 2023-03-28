ce77ff419e72f30946e5b60472df6614ac5f8a87
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-27 21:58:23 -0300

    consigna de reto agregada

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,8 +1,6 @@
 /*
     Añade un campo extra llamado CC, para añadir un destinatario extra.
     este campo no es obligatorio pero en caso de tener informacion debes validar que sea un email valido
-
-    este reto te debe tomar 20min
 */
 
 document.addEventListener('DOMContentLoaded', function () {
