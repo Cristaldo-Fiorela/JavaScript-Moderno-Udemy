c09438162bb6b40371c51b38a931d5f25546e60f
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-03-30 00:45:16 -0300

    filtros terminados y funcionando

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -43,18 +43,23 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     minimo.addEventListener('change', e => {
         datosBusqueda.minimo = e.target.value;
+        filtrarAuto();
     })
     maximo.addEventListener('change', e => {
         datosBusqueda.maximo = e.target.value;
+        filtrarAuto();
     })
     puertas.addEventListener('change', e => {
         datosBusqueda.puertas = e.target.value;
+        filtrarAuto();
     })
     transmision.addEventListener('change', e => {
         datosBusqueda.transmision = e.target.value;
+        filtrarAuto();
     })
     color.addEventListener('change', e => {
         datosBusqueda.color = e.target.value;
+        filtrarAuto();
     })
 
 // Funciones
@@ -92,7 +97,7 @@ function llenarSelect() {
 
 // Filtros de select
 function filtrarAuto() {
-    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear );
+    const resultado = autos.filter( filtrarMarca ).filter( filtrarYear ).filter( filtrarMinimo).filter( filtrarMaximo).filter( filtrarPuertas).filter( filtrarTransmision ).filter( filtrarColor );
 
     mostrarAutos(resultado);
 }
@@ -114,6 +119,50 @@ function filtrarYear(auto) {
     return auto;
 }
 
+function filtrarMinimo(auto) {
+    const { minimo } = datosBusqueda;
+    
+    if(minimo) {
+        return auto.precio >= minimo;
+    }
+    return auto;
+}
+
+function filtrarMaximo(auto) {
+    const { maximo } = datosBusqueda;
+    
+    if(maximo) {
+        return auto.precio <= maximo;
+    }
+    return auto;
+}
+
+function filtrarPuertas(auto) {
+    const { puertas } = datosBusqueda;
+
+    if(puertas) {
+        return auto.puertas == puertas;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto) {
+    const { transmision } = datosBusqueda;
+
+    if(transmision) {
+        return auto.transmision == transmision;
+    }
+    return auto;
+}
+
+function filtrarColor(auto) {
+    const { color } = datosBusqueda;
+
+    if(color) {
+        return auto.color == color;
+    }
+    return auto;
+}
 /* 
 Ver desde 17 a 21
 */
