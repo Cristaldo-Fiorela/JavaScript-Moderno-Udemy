7e716b5121df307b738135edc193bddefc9b0c4c
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-04-10 18:00:10 -0300

    heredar una clase que esta siento importada

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,4 +1,6 @@
 import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente } from './cliente.js';
+import { Empresa } from './empresa.js'
+
 
 console.log(nombreCliente, ahorro);
 console.log(mostrarInformacion(nombreCliente, ahorro));
@@ -8,3 +10,7 @@ tieneSaldo(ahorro)
 const cliente = new Cliente(nombreCliente, ahorro);
 
 console.log(cliente.mostrarInformacion());
+
+//Importar empresa 
+const empresa = new Empresa('Codigo con Juan', 100, 'Aprendizaje en linea');
+console.log(empresa);
