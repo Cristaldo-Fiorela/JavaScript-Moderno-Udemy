83add077282550fd4581f78e5820ddff3fcc5771
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-04-10 17:54:41 -0300

    exportar e importar una clase

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,6 +1,10 @@
-import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo } from './cliente.js';
+import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente } from './cliente.js';
 
 console.log(nombreCliente, ahorro);
 console.log(mostrarInformacion(nombreCliente, ahorro));
 
 tieneSaldo(ahorro)
+
+const cliente = new Cliente(nombreCliente, ahorro);
+
+console.log(cliente.mostrarInformacion());
