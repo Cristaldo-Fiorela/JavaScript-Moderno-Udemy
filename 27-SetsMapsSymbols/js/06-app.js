73c02810bd0d0ae0f213e5710d58b62151a9ff44
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-04-10 16:26:27 -0300

    comentando nuestra funcion de crear un iterador desde 0

--- 27-SetsMapsSymbols/js/06-app.js
+++ 27-SetsMapsSymbols/js/06-app.js
@@ -1,12 +1,17 @@
 const carrito = ['Producto 1', 'Producto 2', 'Producto 3']
 
 function crearIterador() {
+    // Definiendo nuesto contador inicial
     let i = 0;
 
     return {
         siguiente: () => {
+            // evalua el fin del carro con true-false
             const fin = ( i >= carrito.length);
+            // retorna el valor actual del carrito
+            // "si no llegamos al final queremos obtener el valor de nuestra posicion actual, de lo contrario si llegamos al final devolvemos undefined"
             const valor = !fin ? carrito[i++]: undefined;
+
             return {
                 fin, 
                 valor,
@@ -15,7 +20,7 @@ function crearIterador() {
     }
 }
 
-// Utilizar le iterador
+// Utilizar la funcion del iterador "crearIterador" que retorna nuestra funcion de "siguiente"
 const recorrerCarrito = crearIterador(carrito);
 
 console.log(recorrerCarrito.siguiente()); // posicion 0
