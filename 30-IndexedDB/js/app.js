5e09dae2c3b40c133b8b88b7c1dbf05ccf07533e
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-04-11 17:06:01 -0300

    creando un nuevo cliente por medio de una transaccion

--- 30-IndexedDB/js/app.js
+++ 30-IndexedDB/js/app.js
@@ -1,5 +1,11 @@
+let DB;
+
 document.addEventListener('DOMContentLoaded', () => {
     crmDB();
+
+    setTimeout(() => {
+        crearCliente();
+    }, 5000)
 })
 
 function crmDB() {
@@ -14,9 +20,55 @@ function crmDB() {
     //Si se creo bien
     crmDB.onsuccess = function() {
         console.log('Base de datos creada!');
+
+        DB = crmDB.result
     }
     //Configuracion de la base de datos
-    crmDB.onupgradeneeded = function () {
-        console.log('Este metodo solo se ejecuta una vez....');
+    crmDB.onupgradeneeded = function(e) {
+        const db = e.target.result;
+        console.log(db);
+
+
+        // Permite crear las columnas de nuestra base de datos
+        const objectStore = db.createObjectStore('crm', {
+            // creamos una configuracion de como queremos que sea nuestra base de datos
+            keyPath: 'crm',
+            // al agregar uno auto incrementa el id
+            autoIncrement: true,
+        })
+        
+        // Definir las columnas
+        // createIndex(CMR, keypath(nombre con el que hacemos referencia para consultar la tabla buscada), opciones { dato unico: si o no} )
+        objectStore.createIndex('nombre','nombre', { unique: false });
+        objectStore.createIndex('email','email', { unique: true });
+        objectStore.createIndex('telefono','telefono', { unique: false });
+
+        console.log('Columnas Creadas')
     }
+}
+
+function crearCliente() {
+    let transaction =  DB.transaction(['crm'], 'readwrite'); // readonly para solo leer
+
+    transaction.oncomplete = function() {
+        console.log('Transaccion Completada');
+    }
+
+    transaction.onerror = function() {
+        console.log('Hubo un error en la transaccion');
+    }
+
+    const objectStore = transaction.objectStore('crm');
+
+    const nuevoCliente = {
+        telefono: 1238907,
+        nombre: 'fiore',
+        email: 'fiore@gmail.com',
+    }
+
+    const peticion = objectStore.add(nuevoCliente);
+    // const actualiza = objectStore.put(nuevoCliente);    
+    // const elimina = objectStore.delete(nuevoCliente);
+
+    console.log(peticion);
 }
