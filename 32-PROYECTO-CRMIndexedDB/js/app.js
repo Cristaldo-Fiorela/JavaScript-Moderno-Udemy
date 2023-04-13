a8383902ff1185c1932f958cdae4a94cfb6d6b4c
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-04-13 20:00:38 -0300

    imprimiendo los clientes en el HTML, botones de editar y eliminar todavia sin funcionalidad

--- 32-PROYECTO-CRMIndexedDB/js/app.js
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -1,5 +1,6 @@
 (function () {
     let DB;
+    const contenedorCliente = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
@@ -9,30 +10,94 @@
     function crearDB() {
         const crearDB = window.indexedDB.open('crm', 1);
 
-        crearDB.onerror = function() {
+        crearDB.onerror = function () {
             console.log('Hubo un error');
         };
 
-        crearDB.onsuccess = function() {
+        crearDB.onsuccess = function () {
             DB = crearDB.result;
+
+            imprimirClientes();
         }
 
-        crearDB.onupgradeneeded = function(e){
+        crearDB.onupgradeneeded = function (e) {
             const db = e.target.result;
 
-            const objectStore = db.createObjectStore('crm', { 
-                keyPath: 'id', 
-                autoIncrement: true 
+            const objectStore = db.createObjectStore('crm', {
+                keyPath: 'id',
+                autoIncrement: true
             });
 
-            objectStore.createIndex('nombre', 'nombre',  { unique: false });
-            objectStore.createIndex('email', 'email',  { unique: true });
-            objectStore.createIndex('telefono', 'telefono',  { unique: false });
-            objectStore.createIndex('empresa', 'empresa',  { unique: false });
-            objectStore.createIndex('id', 'id',  { unique: true });
+            objectStore.createIndex('nombre', 'nombre', { unique: false });
+            objectStore.createIndex('email', 'email', { unique: true });
+            objectStore.createIndex('telefono', 'telefono', { unique: false });
+            objectStore.createIndex('empresa', 'empresa', { unique: false });
+            objectStore.createIndex('id', 'id', { unique: true });
         }
 
         console.log('Db Lista y creada');
+    }
+
+    function imprimirClientes() {
+        // Leyendo el contendio de la base de datos
+        const objectStore = DB.transaction('crm').objectStore('crm');
+
+        // openCursos actua como iterador de la respuesta de mi base de datos
+        objectStore.openCursor().onsuccess = function (e) {
+            //console.log(e.target.result) // consultamos si los datos estan entrando bien
+
+            const cursor = e.target.result;
+            if (cursor) {
+                //console.log(cursor.value) // todo ok
+
+                const { nombre, telefono, empresa, id } = cursor.value;
+
+                // Creamos un elemento contenedor donde imprimir nuestros datos ಠ_ಠ
+                const tableRowCliente = document.createElement('TR');
+                tableRowCliente.dataset.id = id;
+
+                // Modificamos el HTML de la ultima columna para que permita dos TD juntos
+                const accionesTableHeading = document.querySelector('th').nextElementSibling.nextElementSibling.nextElementSibling;
+                //console.log(accionesTableHeading)
+                accionesTableHeading.setAttribute('colspan', '2');
+
+                //Creando los elementos que debemos rellenar (●'◡'●)
+                const nombreCliente = document.createElement('TD');
+                nombreCliente.innerHTML = `${nombre}`
+
+                const telefonoCliente = document.createElement('TD');
+                telefonoCliente.innerHTML = `${telefono}`
+
+                const empresaCliente = document.createElement('TD');
+                empresaCliente.innerHTML = `${empresa}`
+
+                // Agregando el boton de editar...
+                const accionesTDuno = document.createElement('TD');
+                const btnEditar = document.createElement('BUTTON');
+                btnEditar.innerHTML = 'Editar';
+
+                // Agregando el boton de eliminar...
+                const accionesTDdos = document.createElement('TD');
+                const btnEliminar = document.createElement('BUTTON');
+                btnEliminar.innerHTML = 'Eliminar';
+
+                //AGREGANDO AL HTML
+                accionesTDuno.appendChild(btnEditar);
+                accionesTDdos.appendChild(btnEliminar);
+
+                tableRowCliente.appendChild(nombreCliente);
+                tableRowCliente.appendChild(telefonoCliente);
+                tableRowCliente.appendChild(empresaCliente);
+                tableRowCliente.appendChild(accionesTDuno);
+                tableRowCliente.appendChild(accionesTDdos);
+
+                contenedorCliente.appendChild(tableRowCliente);
+
+                //Va al sgte elemento 
+                cursor.continue();
+            }
+
+        }
     }
 
 })();
