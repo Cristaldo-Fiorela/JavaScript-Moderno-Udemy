f8fa6dca6a4563873122497ef72423b751900cf8
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-04-14 15:47:07 -0300

    obteniendo los clientes con el metodo del profe

--- 32-PROYECTO-CRMIndexedDB/js/app.js
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -1,9 +1,13 @@
 (function () {
     let DB;
-    const contenedorCliente = document.querySelector('#listado-clientes');
+    //const contenedorCliente = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
+
+        if (window.indexedDB.open('crm', 1)) {
+            obtenerClientesProfe();
+        }
     })
 
     //Crear la base de datos
@@ -16,8 +20,6 @@
 
         crearDB.onsuccess = function () {
             DB = crearDB.result;
-
-            imprimirClientes();
         }
 
         crearDB.onupgradeneeded = function (e) {
@@ -96,7 +98,50 @@
                 //Va al sgte elemento 
                 cursor.continue();
             }
+        }
+    }
 
+    function obtenerClientesProfe() {
+        const abrirConexion = window.indexedDB.open('crm', 1);
+
+        abrirConexion.onerror = function() {
+            console.log('Hubo un error');
+        }
+
+        abrirConexion.onsuccess = function() {
+            DB = abrirConexion.result;
+            const objectStore = DB.transaction('crm').objectStore('crm');
+
+            objectStore.openCursor().onsuccess = function(e) {
+                const cursor = e.target.result;
+
+                if (cursor) {
+                    const { nombre, empresa, email, telefono, id } = cursor.value;
+    
+                    const listadoClientes = document.querySelector('#listado-clientes');
+                    listadoClientes.innerHTML += ` 
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                                <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                                <p class="text-gray-700">${telefono}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                                <p class="text-gray-600">${empresa}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                                <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                            </td>
+                        </tr>
+                        `;
+                    cursor.continue();
+                } else {
+                    console.log('No hay mas registros');
+                }
+            }
         }
     }
 
