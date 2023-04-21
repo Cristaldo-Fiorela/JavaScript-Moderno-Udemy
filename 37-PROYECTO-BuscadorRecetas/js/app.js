9d3b4ff63e6c52fa3466e60777777fb6d905549a
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-04-21 16:33:15 -0300

    mostrando informacion en el modal de bootstrap

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -2,6 +2,8 @@
 const selectCategories = document.querySelector('#categorias');
 const resultado =  document.querySelector('#resultado');
 
+const modal = new bootstrap.Modal('#modal', {});
+
 // Event Listener
 document.addEventListener('DOMContentLoaded', iniciarApp);
 selectCategories.addEventListener('change', seleccionarCategoria);
@@ -74,6 +76,11 @@ function mostrarRecetas(recetas = []) {
         const recetaBtn = document.createElement('BUTTON');
         recetaBtn.classList.add('btn', 'btn-danger', 'w-100');
         recetaBtn.textContent = 'Ver Receta';
+        // recetaBtn.dataset.bsTarget = '#modal';
+        // recetaBtn.dataset.bsToggle = 'modal';
+        recetaBtn.onclick = function () {
+            seleccionarReceta(idMeal);
+        }
 
         //Inyectar en el codigo HTML
         recetaCardBody.appendChild(recetaHeading);
@@ -88,6 +95,32 @@ function mostrarRecetas(recetas = []) {
     })
 }
 
+function seleccionarReceta(id) {
+    const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then( resultado => mostrarRecetaModal(resultado.meals[0]))
+
+}
+
+function mostrarRecetaModal(receta) {
+    const {idMeal, strInstructions, strMeal, strMealThumb} = receta;
+
+    // Añador contenido al modal
+    const modalTitle = document.querySelector('.modal .modal-title');
+    const modalBody = document.querySelector('.modal .modal-body');
+
+    modalTitle.textContent = strMeal;
+    modalBody.innerHTML = `
+        <img class="img-fluid" src=${strMealThumb} alt="receta ${strMeal}" />
+        <h3>Instrucciones</h3>
+        <p>${strInstructions}</p>
+    `;
+
+    //Muestra el modal
+    modal.show();
+}
 function limpiarHTML(selector) {
     while(selector.firstChild) {
         selector.removeChild(selector.firstChild);
