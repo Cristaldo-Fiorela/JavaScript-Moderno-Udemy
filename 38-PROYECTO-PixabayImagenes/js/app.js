8c1fac7d3ac96761925640fa830e0a795f025917
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-04-24 17:27:38 -0300

    mostrar los resultados de la consulta

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -42,7 +42,7 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
     const key = '35736069-6ee8dfb84528c64aa6d670fae';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
 
     fetch(url)
         .then( respuesta => respuesta.json())
@@ -50,5 +50,29 @@ function buscarImagenes(termino) {
 }
 
 function mostrarImagenes(imagenes) {
-    console.log(imagenes)
+    console.log(imagenes);
+
+    // Limpia el HTML
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Iterar sobre el arreglo de imagenes y construir en el HTML
+    imagenes.forEach( imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4"> 
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}" >
+                    <div class="p-4">
+                        <p class="font-bold"> ${likes} <span class="font-light"> Me Gusta</span></p>
+                        <p class="font-bold"> ${views} <span class="font-light"> Veces vista</span></p>
+
+                        <a class="block w-full bd-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1" href="${largeImageURL}" target="_blank" rel="noopener noreferrer">Ver imagen</a>
+                    </div>
+                </div>
+            </div>
+        `
+    });
 }
