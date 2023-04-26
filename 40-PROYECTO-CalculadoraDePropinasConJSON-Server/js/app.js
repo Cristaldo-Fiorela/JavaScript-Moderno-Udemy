9167426704b49bf25830e5f8f69df58a301c0970
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-04-26 15:56:19 -0300

    mostransdo la informacion de la API en el HTML + agregando un input para que el usuario pueda elegir la cantidad de los productos

--- 40-PROYECTO-CalculadoraDePropinasConJSON-Server/js/app.js
+++ 40-PROYECTO-CalculadoraDePropinasConJSON-Server/js/app.js
@@ -4,6 +4,11 @@ let cliente = {
     pedido: [],
 }
 
+const categorias = {
+    1: 'Comida',
+    2: 'Bebida',
+    3: 'Postres'
+}
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', guardarCliente);
 
@@ -30,5 +35,74 @@ function guardarCliente() {
         }
         return;
     }
-    console.log('Todos los campos estan llenos')
+
+    // Asignando datos del formulario a nuestro objeto de cliente
+    cliente = { ...cliente, mesa, hora,};
+
+    //Ocultar Modal
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+
+    // Mostrar las secciones
+    mostrarSecciones();
+
+    //Obtenes Platillos de la PI de JSON-Server
+    obtenerPlatillo();
+}
+
+function mostrarSecciones() {
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach( seccion => seccion.classList.remove('d-none'));
+}
+
+function obtenerPlatillo() {
+    const url = 'http://localhost:4000/platillos'
+
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then( resultado => mostrarPlatillos(resultado))
+        .catch( error => console.log(error))
+}
+
+function mostrarPlatillos(platillos) {
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platillos.forEach( platillo => {
+        const { nombre, precio, categoria, id } = platillo;
+
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+
+        const nombrePlatillo = document.createElement('DIV');
+        nombrePlatillo.classList.add('col-md-4');
+        nombrePlatillo.textContent = nombre;
+
+        const precioPlatillo = document.createElement('DIV');
+        precioPlatillo.classList.add('col-md-3', 'fw-bold');
+        precioPlatillo.textContent = `$${precio}`;
+
+        const categoriaPlatillo = document.createElement('DIV');
+        categoriaPlatillo.classList.add('col-md-3');
+        categoriaPlatillo.textContent = categorias[categoria];
+
+        const inputCantidad = document.createElement('INPUT');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `producto-${id}`;
+        inputCantidad.classList.add('form-control');
+
+        const agregar = document.createElement('DIV');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(inputCantidad);
+
+        // Imprimiendo en el HTML
+        row.appendChild(nombrePlatillo);
+        row.appendChild(precioPlatillo);
+        row.appendChild(categoriaPlatillo);
+        row.appendChild(agregar);
+
+        contenido.appendChild(row);
+    })
 }
