cac7c6939f6236773ae95fd4afa328768ff42c9a
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-05-01 17:21:21 -0300

    mostrando los clientes de nuestra base de datos en nuestro HTML

--- 44-PROYECTO-CRM-CRUD-REST/js/API.js
+++ 44-PROYECTO-CRM-CRUD-REST/js/API.js
@@ -1,5 +1,6 @@
 const url = 'http://localhost:4000/clientes';
 
+// Cuando se crea un nuevo cliente
 export const nuevoCliente = async cliente => {
     console.log(cliente)
 
@@ -14,5 +15,16 @@ export const nuevoCliente = async cliente => {
         window.location.href = 'index.html';
     } catch (error) {
         
+    }
+}
+
+// Obtiene nuevo cliente 
+export const obtenerClientes = async () => {
+    try {
+        const resultado = await fetch(url);
+        const clientes = await resultado.json();
+        return clientes;
+    } catch (error) {
+        console.log(error)
     }
 }
