1e185c23ab5dc54544d96798737070959ba1b586
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-05-03 15:57:18 -0300

    agregando un manifest creado para mejorar el reporte de PWA

---
+++ 47-ServiceWorkers-PWA/js/app.js
@@ -0,0 +1,18 @@
+/*
+Progresive web application
+Son rapidas - en menos de 5seg cargan
+Instalable - en telefono o computadora
+Conexion offline 
+
+Service Workes
+-Son script corriendo todo el tiempo detras de escena
+-Funcionan Offline
+- No tienen acceso al DOM
+-Cargan defroma instantanea
+-Sincronizan los datos detras de escena o sin interferir en la navegacion
+
+Funciones no disponibles en Service Workes
+- window(self)
+-document(caches)
+-local storage (fetch)
+ */
