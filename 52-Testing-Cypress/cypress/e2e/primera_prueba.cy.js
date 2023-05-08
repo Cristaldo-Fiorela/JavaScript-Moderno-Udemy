7fb80bc38aa15f65bb8c115bf3bdfc5d948719b0
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-05-08 16:35:39 -0300

    manejo de atributos en cypress

--- 52-Testing-Cypress/cypress/e2e/primera_prueba.cy.js
+++ 52-Testing-Cypress/cypress/e2e/primera_prueba.cy.js
@@ -6,9 +6,26 @@ describe('Carga la pagina principal', () => {
     cy.visit('http://127.0.0.1:5500/52-Testing-Cypress/index.html');
   
     // Busca si la pagina que visita coincide con la busqueda acceciendo despues de  visitar la web
+    // Verifica su elemento y texto
     cy.contains('h1', 'Administrador de Pacientes de Veterinaria');
 
-    // Busqueda de elemento mas generalizada.
-    cy.get('h1').should('exist');
+    // Busqueda de elemento mas generalizada. Get obtiene elemento de DOM y comprueba su contenido.
+    // Verifica si existe
+    //cy.get('h1').should('exist'); // ! Mala practica porque es muy general
+    cy.get('[data-cy="titulo-proyecto"]').should('exist'); // buena practica porque es mas especifico en lo que busca
+
+    // Verificar que exista el elemento y contenga un texto en especifico
+    cy.get('[data-cy="titulo-proyecto"]')
+      .invoke('text')
+      .should('equal', 'Administrador de Pacientes de Veterinaria');
+
+    // Verificar el texto de las citas
+    cy.get('[data-cy=citas-heading]')
+      .invoke('text')
+      .should('equal', 'No hay Citas, comienza creando una');
+
+    cy.get('[data-cy=citas-heading]')
+      .invoke('text')
+      .should('not.equal', 'Fiorela');
   })
 })
