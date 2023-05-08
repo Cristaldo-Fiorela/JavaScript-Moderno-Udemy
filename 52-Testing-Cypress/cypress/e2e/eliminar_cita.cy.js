a5fa9cc2e3178f001c194d462fa9aa0c0b0c06cd
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-05-08 18:13:38 -0300

    Eliminar citas con cypress

---
+++ 52-Testing-Cypress/cypress/e2e/eliminar_cita.cy.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+
+describe('Llena los campos para una nueva cita y la elimina', () => {
+  beforeEach('Campos nueva cita', () => {
+    // Visita el sitio web
+    cy.visit('/index.html');
+
+    cy.get('[data-cy="mascota-input"]')
+      .type('Donna'); // escribe en el input
+
+    cy.get('[data-cy="propietario-input"]')
+      .type('Fiorela');
+
+    cy.get('[data-cy="telefono-input"]')
+      .type('123987312');
+
+    cy.get('[data-cy="fecha-input"]')
+      .type('2023-05-08');
+
+    cy.get('[data-cy="hora-input"]')
+      .type('20:30');
+
+    cy.get('[data-cy="sintomas-textarea"]')
+      .type('Duerme mucho');
+
+    cy.get('[data-cy="submit-cita"]')
+      .click();
+
+    cy.get('[data-cy=citas-heading]')
+      .invoke('text')
+      .should('equal', 'Administra tus Citas');
+
+    // Seleccionar la alerta
+    cy.get('[data-cy=alerta]')
+      .invoke('text')
+      .should('equal', 'Se agregó correctamente');
+
+    cy.get('[data-cy=alerta]')
+      .should('have.class', 'alert-success');
+  });
+
+  it('Eliminar una cita', () => {
+    cy.get('[data-cy=btn-eliminar]')
+      .click();
+    
+    cy.get('[data-cy=citas-heading]')
+      .invoke('text')
+      .should('equal', 'No hay Citas, comienza creando una');
+  });
+});
