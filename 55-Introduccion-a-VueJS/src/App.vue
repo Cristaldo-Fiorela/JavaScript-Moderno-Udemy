25b3ae2022a9a51eab10598bb5d734f49d5ddd8e
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-05-22 17:39:59 -0300

    modificando el state de reactive

--- 55-Introduccion-a-VueJS/src/App.vue
+++ 55-Introduccion-a-VueJS/src/App.vue
@@ -1,13 +1,32 @@
 <script setup>
-import Header from './components/Header.vue';
+import { ref } from "vue";
+import Header from "./components/Header.vue";
+
+const cantidad = ref(0)
+// const state = reactive({ // no toma valores primitivos
+//   cantidad: 0
+// })
+
+function handleChange(e) {
+  cantidad.value = +e.target.value;
+}
+
+console.log(cantidad.value);
 </script>
 
 <template>
-  <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
-    <Header />
-  </div>
+    <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+        <Header />
+        <div class="my-5">
+            <input
+                type="range"
+                class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+                @input="handleChange"
+            />
+
+            {{ cantidad }}
+        </div>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
