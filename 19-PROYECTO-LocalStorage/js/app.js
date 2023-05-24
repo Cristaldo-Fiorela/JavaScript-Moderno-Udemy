f92859c29540512b2f8a937e2dec1569e23bffd1
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-05-24 17:10:16 -0300

    mostrando los tweets en el HTML

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -13,18 +13,29 @@ function eventListeners() {
 // Funciones
 function agregarTweet(e) {
     e.preventDefault();
-    
+
     // Textarea donde el usuario escribe
     const tweet = document.querySelector('#tweet').value;
 
     //Validacion
-    if(tweet === '') {
+    if (tweet === '') {
         mostrarError('Un mensaje no puede ir vacio');
-        
-        return; // evita que se ejecutan mas lineas de codigo
-    } 
 
-    console.log('AGREGANDO TWEet');
+        return; // evita que se ejecutan mas lineas de codigo
+    }
+
+    const tweetObj = {
+        id: Date.now(),
+        tweet
+    }
+    // Añadir al arreglo de tweets
+    tweets = [...tweets, tweetObj];
+
+    // Una vez agregado
+    crearHTML();
+
+    // Reiniciar el formulario
+    formulario.reset();
 };
 
 // Mostrar mensaje de eror
@@ -36,9 +47,36 @@ function mostrarError(mensaje) {
     // Insertando en el contenido
     const contenido = document.querySelector('#contenido');
     contenido.appendChild(mensajeError);
-    
+
     // Elimina la alerta despues de 3 segundos
     setTimeout(() => {
         mensajeError.remove();
     }, 3000);
+}
+
+// Muestra un listado de los tweets
+function crearHTML() {
+
+    limpiarHTML();
+
+    if (tweets.length > 0) {
+        tweets.forEach(tweet => {
+            // Crear el HTML
+
+            const li = document.createElement('LI');
+
+            // Añadir el texto
+            li.innerHTML = tweet.tweet;
+
+            // Insertarlo en el HTML
+            listaTweets.appendChild(li);
+        });
+    }
+}
+
+//Limpiar el HTML
+function limpiarHTML() {
+    while (listaTweets.firstChild) {
+        listaTweets.removeChild(listaTweets.firstChild);
+    }
 }
